898bd2cbc1c3aee90c12b5aa4ebb2f80c874b1d3
Waylon Williams <waylonwilliams@Waylons-MacBook-Pro.local>
2023-10-28 05:52:12 -0700

    flask setup

--- app.py
+++ app.py
@@ -4,10 +4,12 @@
 from flask import Flask
 from flask_cors import CORS
 from main import main
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24).hex()
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}) # asssuming react server is running here
 app.register_blueprint(main, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from flask import Blueprint, request 
+# TO DO
+# audio from react to flask
+# write prompts for each emotion
+# create text editor page after flask has analyzed the sigh
+# database to save user journals (mongo db)
+# another option to have hume analyze your journals
+
+
+from flask import Blueprint, request, redirect, session
+import json
+import os
+import base64
+from binary import lll
 
 main = Blueprint(__name__, "plan")
+
 
 from hume import HumeBatchClient
 from hume.models.config import FaceConfig
 from hume.models.config import ProsodyConfig
 
-
-client = HumeBatchClient("MCuO1K3e33jT4rYJT1iqQc7CBxWoCzJ8KxGFkRLVIHbA0XJ0")
-urls = []
+client = HumeBatchClient("pP0Q67fHYxCnZUTy9Ov71kCBXw6vzglYABCogBJkTooXAWfj")
 configs = [ProsodyConfig(granularity="utterance")] # this uses prosody, was more interested in sounds, can explore more tomorrow
 
 
@@ -16,17 +27,41 @@ configs = [ProsodyConfig(granularity="utterance")] # this uses prosody, was more
 def base():
     return "test"
 
-@main.route("/audio", methods=["POST"])
+
+
+@main.route("/audio")
 def audio():
     # ill need to process whatever data you send here
-    urls.append("https://storage.googleapis.com/hume-test-data/video/armisen-clip.mp4") # assumes you will be passing a url, can also be adjusted
-    job = client.submit_job(urls, configs)
+    #data = request.json()
+    # with axios
+    #file = request.form["file"]
+    #filename = request.form["fileName"]
+    audio_b64 = lll # request.get_json()
+    audio_clean = base64.b64decode(audio_b64)
+    with open("audio.webm", "wb") as fh:
+        fh.write(audio_clean)
+    session["urls"] = ["https://storage.googleapis.com/hume-test-data/video/armisen-clip.mp4"] # just change it to local file
+    job = client.submit_job(session["urls"], configs)
     print(job)
     print("Running...")
     job.await_complete()
     job.download_predictions("predictions.json")
-    print("Predictions downloaded to predictions.json")
-    return
+    with open("predictions.json", "r") as fh:
+        emotions = json.load(fh)
+    emotions = emotions[0]["results"]["predictions"][0]["models"]["prosody"]["grouped_predictions"][0]["predictions"][0]["emotions"]
+    print(emotions)
+    max_index = 0
+    max_emotion = 0
+    for i in range(len(emotions)):
+        if emotions[i]["score"] > max_emotion:
+            max_emotion = emotions[i]["score"]
+            max_index = i
+    print("Max = ", emotions[max_index])
+
+    # resetting for next user, should be in flask session imo
+    os.remove("predictions.json")
+
+    return redirect("/")   
 
 @main.route("/journal", methods=["POST"])
 def journal():
