0c9e5247e235468ef716d81a19b22f1f2cd8468c
Waylon Williams <waylonwilliams@Waylons-MacBook-Pro.local>
2023-10-28 21:41:38 -0700

    hopefully final backend comit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # TO DO
-# database to save user journals (mongo db)
-# another option to have hume analyze your journals
+# chat gpt analysis of journals
+# home button react
+# journals page react
+#     chat gpt button on that page -> another page
+#     journals link to themselves in text editor, I could supply the content that makes up specific journal id?
 
 
 from flask import Blueprint, request, jsonify
@@ -34,7 +37,7 @@ client = HumeBatchClient("N8s5670QNccUlXE5S3uOydcR8Q2EWMP2dWGkXG0NIx4Sf86v")
 configs = [ProsodyConfig(granularity="utterance"), BurstConfig()] # this uses prosody, was more interested in sounds, can explore more tomorrow
 urls = ["audio.webm"]
 
-chat_key = "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"
+chat_key = "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"
 openai.api_key = chat_key
 
 async def get_emotion():
@@ -56,18 +59,15 @@ def audio():
 
     # gets emotion scores from live api
     emotions = asyncio.run(get_emotion())
-    print("\n\n\n Base emotions ", emotions)
     if "burst" in emotions:
         emotions = emotions["burst"]["predictions"][0]["emotions"]
     elif "prosody" in emotions:
         emotions = emotions["prosody"]["predictions"][0]["emotions"]
     else:
         emotions = [{"name": "Calmness", "score": 1}]
-    print("\n\n\n", emotions)
     max_index = 0
     max_emotion = 0
     for i in range(len(emotions)):
-        print(emotions[i]["name"])
         if emotions[i]["score"] > max_emotion:
             max_emotion = emotions[i]["score"]
             max_index = i
@@ -106,7 +106,7 @@ def journal_get():
 @main.route("/add_journal", methods=["POST"])
 def add_journal():
     data = request.get_json()
-    # make a heading
+    # make  a heading
     name = ""
     i = 0
     while i < len(data["content"]):
@@ -128,7 +128,7 @@ def add_journal():
     # prompt content user name(first 20 of paragraph)
 
 
-@main.route("/journal_analysis") # may need to be post to get user
+@main.route("/journal_analysis") # post in order to get user
 def journal_analysis():
     all_journal_data = journals.find({"user": "Waylon"}) # replace with current user
     journal_array = []
@@ -142,29 +142,49 @@ def journal_analysis():
                 i += 1
                 continue
             name += journal["content"][i]
-            if len(name) >= 20:
-                break
             i += 1
         journal_array.append("Prompt:")
         journal_array.append(journal["prompt"])
         journal_array.append("Response:")
         journal_array.append(name)
     # send to chat
-    print(journal_array)
-    journal_array = journal_array.join(". ")
-    print(journal_array)
+    journal_array = " ".join(journal_array)
 
 
 
-    response = openai.ChatCompletion.create(
+    chat_summary = openai.ChatCompletion.create(
     model="gpt-4",
     messages=[ {
-      "role": "user",
-      "content": "Summarize this string of journal entries: "
-      } ],
-    temperature=1,
-    max_tokens=512,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+        "role": "user",
+        "content": "I am going to provide you with a string of journal prompts and responses. Analyze the string and provide insightful information about how the user processes their emotions. Make specific comments on improvements they can make and possible things they aren't considering. Praise the user for good things they have done which were morally just. Refer to things they have done like 'You are very animal friendly.'"
+        },
+        {
+        "role": "assistant",
+        "content": "Without the specific string of prompts and responses, it's impossible to provide any sort of analysis or feedback. Please supply the said string so I can provide proper assistance."
+        },
+        {
+        "role": "user",
+        "content": "Here is the string: {}".format(journal_array)
+        }, ],
+        temperature=1,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
+
+    chat_specific = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[ {
+        "role": "user",
+        "content": "I am going to provide you with a string of journal prompts and responses. Pick out as many specific good things as you can that occurred throughout their journals. Write as if you are speaking to them, like \"You did this.\" Here is the string: {}".format(journal_array)
+        }, ],
+        temperature=1,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+    chat_summary = chat_summary["choices"][0]["message"]["content"]
+    chat_specific = chat_specific["choices"][0]["message"]["content"]
+    return {"summary": str(chat_summary), "events": str(chat_specific)}
